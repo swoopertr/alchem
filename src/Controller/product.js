670b5bc057af3003b1404ddc2fb4d55e485884b4
ariftunckiral <tunc.kiral@fiyuu.com.tr>
2022-03-04 04:07:04 +0300

    .

--- src/Controller/product.js
+++ src/Controller/product.js
@@ -60,15 +60,11 @@ var product = {
         }
         try {
             global.events.once(token + 'form_posted_end', function(){
-                console.log('form_posted_end');
                 productBussines.ins_update_product(req.formData, 
                     function(result){
-                        render.renderData(res, {
-                            "success": "true", 
-                            "result": result
-                        });
+                        core.redirect(res, '/admin/urunler');
                     },
-                    function(){
+                    function(err){
                         render.renderData(res, {"error": "error"});
                     });
             });    
