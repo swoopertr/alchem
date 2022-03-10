f60be40cb762fe0eb720c73c4f17ed3fd3ece44f
ariftunckiral <tunc.kiral@fiyuu.com.tr>
2022-03-11 02:27:04 +0300

    .

--- UI/assets/scripts/admin_survey_evalute_question.js
+++ UI/assets/scripts/admin_survey_evalute_question.js
@@ -2,5 +2,8 @@ console.log('admin_survey_evalute_question.js');
 
 
 (function() {
-    
+    var message = 'Sorulara verdiğiniz cevaplar elimize ulaşmıştır, \nAlchemLife eğitimlerine katıldığınız için teşekkür ederiz.';
+    document.getElementById('btnUpdSave').addEventListener('click', function(e) {
+        alert(message);
+    });
 })();
