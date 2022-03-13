f9a0372de6eb8ddb0713bedc23dde8e7bd4b6702
ariftunckiral <tunc.kiral@fiyuu.com.tr>
2022-03-14 02:44:58 +0300

    .

--- UI/assets/scripts/admin_fud_page.js
+++ UI/assets/scripts/admin_fud_page.js
@@ -3,5 +3,19 @@ userPageType = 1;
 check_login();
 
 (function() {
-    
+    $('.delete_fud').click(function(){
+        var fud_id = $(this).attr('data-id');
+        var token = util.cookie.get('token');
+        var check_obj  = [
+            { name : 'token',         value : token },
+            { name : 'userPageType',  value : userPageType}
+        ];
+        util.postJsonRequest('/admin/fud-sil',{id:fud_id}, check_obj, function(result){
+            if(JSON.parse(result).status == "success"){
+                window.location.href = '/admin/fud-listesi';
+            }else{
+                console.log(result);    
+            }
+        }); 
+    });
 })();
